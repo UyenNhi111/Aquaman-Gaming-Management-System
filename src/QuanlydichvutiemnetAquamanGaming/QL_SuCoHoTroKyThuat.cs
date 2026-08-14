75e959aebdfd4460daead73bf9f38324e688f604
Nhi Uyên <162456135+UyenNhi111@users.noreply.github.com>
2026-08-14 14:06:04 +0700

    Update QL_SuCoHoTroKyThuat.cs

--- src/QuanlydichvutiemnetAquamanGaming/QL_SuCoHoTroKyThuat.cs
+++ src/QuanlydichvutiemnetAquamanGaming/QL_SuCoHoTroKyThuat.cs
@@ -13,7 +13,7 @@ namespace QuanlydichvutiemnetAquamanGaming
 {
     public partial class QL_SuCoHoTroKyThuat : Form
     {
-        string connectionString = @"Data Source=LAPTOP-FFG392QU;Initial Catalog=QLDVTN;Integrated Security=True";
+        string connectionString="Server=YOUR_SERVER;Database=AquamanGaming;Trusted_Connection=True;"
 
         public QL_SuCoHoTroKyThuat()
         {
